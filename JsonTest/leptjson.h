a7e25da2f3517db33253172d647df7c37a83f204
Lee <vleejl@hotmail.com>
2018-11-26 08:20:56 +0800

    finish parse null/true/false and number

--- JsonTest/leptjson.h
+++ JsonTest/leptjson.h
@@ -1,15 +1,10 @@
 #ifndef LEPTJSON_H_
 #define LEPTJSON_H_
 
-#include <memory>
-#include <string.h>
-#include <assert.h>
-
-namespace Leptjson 
+namespace lept
 {
-	enum JsonType 
+	enum class JsonType
 	{
-		NONE_TYPE = -1,
 		JSON_NULL,
 		JSON_FALSE,
 		JSON_TRUE,
@@ -19,47 +14,43 @@ namespace Leptjson
 		JSON_OBJECT
 	};
 
-	enum ParseSstate 
+	struct JsonValue/* \ ÄäÃû struct/union \ */
 	{
-		NONE_PARSE = -1,
-		PARSE_OK = 0, 
+		union
+		{
+			struct
+			{
+				size_t strlen_;
+				char* str_;
+			};
+			double num_;
+		};		
+		JsonType jsonType_;		
+	};
+
+	enum PARSE_STATE
+	{
+		PARSE_OK,
 		PARSE_EXPECT_VALUE,
 		PARSE_INVALID_VALUE,
 		PARSE_ROOT_NOT_SINGULAR,
-		PARSE_NUMBER_TOO_BIG,
+		PARSE_NUMBER_TOO_BIG
 	};
 
-	struct JsonValue
-	{
-		double number_;
-		JsonType jsonType_;
-	};
+#define init(val) do { val.type == JSON_NULL; } while(0)
+	PARSE_STATE parse(JsonValue& value, const char* jsonStr);
+	void free(JsonValue& val);
+	JsonType get_type(JsonValue const& value);
+#define set_null(val) free(val);
 
-	struct JsonContext
-	{
-		const char* jsonStr_;
-	};
+	int get_boolean(JsonValue const& val);
+	void set_boolean(JsonValue& val, int b);
 
-	
-	JsonType getType(const JsonValue& jasonVal);
-	double getNumber(const JsonValue& jasonVal);
+	double get_number(JsonValue const& value);
+	void set_number(JsonValue& val, double n);
 
-#define EXPECT(jsonContext, jsonStr) do {assert(*(jsonContext.jsonStr_) == (jsonStr)); ++(jsonContext.jsonStr_);} while(0)
-
-	/* ws = *(%x20 / %x09 / %x0A / %x0D) */
-	static void parseWhitespace(JsonContext& jsonContext);
-	/* null  = "null" */
-	//static Leptjson::ParseSstate parseNull(JsonContext& jsonContext, JsonValue& jsonVal);
-	///* true  = "true" */
-	//static Leptjson::ParseSstate parseTrue(JsonContext& jsonContext, JsonValue& jsonVal);
-	///* false  = "false" */
-	//static Leptjson::ParseSstate parseFalse(JsonContext& jsonContext, JsonValue& jsonVal);
-	static Leptjson::ParseSstate parseLiteral(JsonContext& jsonContext, JsonValue& jsonVal, const char* concreptLiteral, const JsonType& jsonType);
-	/*  */
-	static Leptjson::ParseSstate parseNumber(JsonContext& jsonContext, JsonValue& jsonVal);
-	/* value = null / false / true */
-	static ParseSstate parseVal(JsonContext& jsonContext, JsonValue& jsonVal);
-	/* parse */
-	ParseSstate parse(JsonValue& jsonVal, const char* jsonStr);
+	const char* get_string(JsonValue& const val);
+	size_t get_string_length(JsonValue& const val);
+	void set_string(JsonValue& val, const char* str, size_t len);
 }
 #endif // !LEPTJSON_H_
--- JsonTest/test.cpp
+++ JsonTest/test.cpp
@@ -22,106 +22,41 @@ static int test_pass = 0;
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 #define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
 
-#define TEST_ERROR(error, json)\
-	do{\
-		Leptjson::JsonValue jsonVal;\
-		jsonVal.jsonType_ = Leptjson::JSON_FALSE;\
-		EXPECT_EQ_INT(error, parse(jsonVal, json));\
-		EXPECT_EQ_INT(Leptjson::JSON_NULL, getType(jsonVal));\
-	} while(0)
 
-static void parseExpectValue()
+
+static void test_parse_null()
 {
-	TEST_ERROR(Leptjson::PARSE_EXPECT_VALUE, "");
-	TEST_ERROR(Leptjson::PARSE_EXPECT_VALUE, " ");
+	lept::JsonValue jsonVal;
+	jsonVal.jsonType_ = lept::JsonType::JSON_NULL;
+	EXPECT_EQ_INT(lept::PARSE_OK, parse(jsonVal, "null"));
+	EXPECT_EQ_INT(lept::JsonType::JSON_NULL, lept::get_type(jsonVal));
 }
 
-static void testParseNull()
+static void test_parse_true()
 {
-	Leptjson::JsonValue jsonVal;
-	jsonVal.jsonType_ = Leptjson::JSON_TRUE;
-	EXPECT_EQ_INT(Leptjson::PARSE_OK, parse(jsonVal, "null"));
-	EXPECT_EQ_INT(Leptjson::JSON_NULL, getType(jsonVal));
+	lept::JsonValue jsonVal;
+	jsonVal.jsonType_ = lept::JsonType::JSON_FALSE;
+	EXPECT_EQ_INT(lept::PARSE_OK, parse(jsonVal, "true"));
+	EXPECT_EQ_INT(lept::JsonType::JSON_TRUE, get_type(jsonVal));
 }
 
-static void testParseTrue()
+static void test_parse_false()
 {
-	Leptjson::JsonValue jsonVal;
-	jsonVal.jsonType_ = Leptjson::JSON_FALSE;
-	EXPECT_EQ_INT(Leptjson::PARSE_OK, parse(jsonVal, "true"));
-	EXPECT_EQ_INT(Leptjson::JSON_TRUE, getType(jsonVal));
-}
-
-static void testParseFalse()
-{
-	Leptjson::JsonValue jsonVal;
-	jsonVal.jsonType_ = Leptjson::JSON_TRUE;
-	EXPECT_EQ_INT(Leptjson::PARSE_OK, parse(jsonVal, "false"));
-	EXPECT_EQ_INT(Leptjson::JSON_FALSE, getType(jsonVal));
-}
-
-static void testParseExpectValue()
-{
-	Leptjson::JsonValue jsonVal;
-	jsonVal.jsonType_ = Leptjson::JSON_FALSE;
-	EXPECT_EQ_INT(Leptjson::PARSE_EXPECT_VALUE, parse(jsonVal, ""));
-	EXPECT_EQ_INT(Leptjson::JSON_NULL, getType(jsonVal));
-}
-
-static void testParseInvalidValue()
-{
-	Leptjson::JsonValue jsonVal;
-	jsonVal.jsonType_ = Leptjson::JSON_FALSE;
-	EXPECT_EQ_INT(Leptjson::PARSE_INVALID_VALUE, parse(jsonVal, "nul"));
-	EXPECT_EQ_INT(Leptjson::JSON_NULL, getType(jsonVal));
-
-	jsonVal.jsonType_ = Leptjson::JSON_FALSE;
-	EXPECT_EQ_INT(Leptjson::PARSE_INVALID_VALUE, parse(jsonVal, "?"));
-	EXPECT_EQ_INT(Leptjson::JSON_NULL, getType(jsonVal));
-
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, "+0");
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, "+1");
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, ".123");
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, "1.");
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, "INF");
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, "inf");
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, "NAN");
-	TEST_ERROR(Leptjson::PARSE_INVALID_VALUE, "nan");
-
-
-}
-
-static void testRootNotSigular()
-{
-	Leptjson::JsonValue jsonVal;
-	jsonVal.jsonType_ = Leptjson::JSON_FALSE;
-	EXPECT_EQ_INT(Leptjson::PARSE_ROOT_NOT_SINGULAR, parse(jsonVal, "null x"));
-	EXPECT_EQ_INT(Leptjson::JSON_NULL, getType(jsonVal));
-
-	TEST_ERROR(Leptjson::PARSE_ROOT_NOT_SINGULAR, "0123");
-	TEST_ERROR(Leptjson::PARSE_ROOT_NOT_SINGULAR, "0x0");
-	TEST_ERROR(Leptjson::PARSE_ROOT_NOT_SINGULAR, "0x123");
-
-}
-
-static void testParseNumberTooBig()
-{
-#if 0
-	TEST_ERROR(Leptjson::PARSE_NUMBER_TOO_BIG, "1e309");
-	TEST_ERROR(Leptjson::PARSE_NUMBER_TOO_BIG, "-1e309");
-#endif // 0
-
+	lept::JsonValue jsonVal;
+	jsonVal.jsonType_ = lept::JsonType::JSON_TRUE;
+	EXPECT_EQ_INT(lept::PARSE_OK, parse(jsonVal, "false"));
+	EXPECT_EQ_INT(lept::JsonType::JSON_FALSE, get_type(jsonVal));
 }
 
 #define TEST_NUMBER(expectNum, actualNum)\
 	do{\
-		Leptjson::JsonValue jsonVal;\
-		EXPECT_EQ_INT(Leptjson::PARSE_OK, parse(jsonVal, actualNum));\
-		EXPECT_EQ_INT(Leptjson::JSON_NUMBER, getType(jsonVal));\
-		EXPECT_EQ_DOUBLE(expectNum, getNumber(jsonVal));\
+		lept::JsonValue jsonVal;\
+		EXPECT_EQ_INT(lept::PARSE_OK, parse(jsonVal, actualNum));\
+		EXPECT_EQ_INT(lept::JsonType::JSON_NUMBER, get_type(jsonVal));\
+		EXPECT_EQ_DOUBLE(expectNum, get_number(jsonVal));\
 	} while(0)
 
-static void testParseNumber()
+static void test_parse_number()
 {
 	TEST_NUMBER(0.0, "0");
 	TEST_NUMBER(0.0, "-0");
@@ -141,23 +76,82 @@ static void testParseNumber()
 	TEST_NUMBER(-1E-10, "-1E-10");
 	TEST_NUMBER(1.234E+10, "1.234E+10");
 	TEST_NUMBER(1.234E-10, "1.234E-10");
-	TEST_NUMBER(0.0, "1e-10000");
+
+	TEST_NUMBER(1.0000000000000002, "1.0000000000000002"); /* the smallest number > 1 */
+	TEST_NUMBER(4.9406564584124654e-324, "4.9406564584124654e-324"); /* minimum denormal */
+	TEST_NUMBER(-4.9406564584124654e-324, "-4.9406564584124654e-324");
+	TEST_NUMBER(2.2250738585072009e-308, "2.2250738585072009e-308");  /* Max subnormal double */
+	TEST_NUMBER(-2.2250738585072009e-308, "-2.2250738585072009e-308");
+	TEST_NUMBER(2.2250738585072014e-308, "2.2250738585072014e-308");  /* Min normal positive double */
+	TEST_NUMBER(-2.2250738585072014e-308, "-2.2250738585072014e-308");
+	TEST_NUMBER(1.7976931348623157e+308, "1.7976931348623157e+308");  /* Max double */
+	TEST_NUMBER(-1.7976931348623157e+308, "-1.7976931348623157e+308");
 }
 
-static void testParse()
+#define TEST_ERROR(error, jsonStr)\
+	do{\
+		lept::JsonValue jsonVal;\
+		jsonVal.jsonType_ = lept::JsonType::JSON_FALSE;\
+		EXPECT_EQ_INT(error, parse(jsonVal, jsonStr));\
+		EXPECT_EQ_INT(lept::JsonType::JSON_NULL, get_type(jsonVal));\
+	} while(0)
+
+static void test_parse_expect_value()
 {
-	testParseNull();
-	testParseTrue();
-	testParseFalse();
-	testParseExpectValue();
-	testParseInvalidValue();
-	testRootNotSigular();
-	testParseNumberTooBig();
+	TEST_ERROR(lept::PARSE_EXPECT_VALUE, "");
+	TEST_ERROR(lept::PARSE_EXPECT_VALUE, " ");
+}
+
+static void test_parse_invalid_value()
+{
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "val");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "?");
+#if 1
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "+0");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "+1");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, ".123");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "1.");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "INF");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "inf");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "NAN");
+	TEST_ERROR(lept::PARSE_INVALID_VALUE, "nan");
+#endif // 0
+}
+
+static void test_root_not_singular()
+{
+	TEST_ERROR(lept::PARSE_ROOT_NOT_SINGULAR, "null x");
+#if 1
+	TEST_ERROR(lept::PARSE_ROOT_NOT_SINGULAR, "0123");
+	TEST_ERROR(lept::PARSE_ROOT_NOT_SINGULAR, "0x0");
+	TEST_ERROR(lept::PARSE_ROOT_NOT_SINGULAR, "0x123");
+#endif // 0
+}
+
+static void test_parse_number_too_big()
+{
+#if 1
+	TEST_ERROR(lept::PARSE_NUMBER_TOO_BIG, "1e309");
+	TEST_ERROR(lept::PARSE_NUMBER_TOO_BIG, "-1e309");
+	TEST_ERROR(lept::PARSE_NUMBER_TOO_BIG, "1e-10000");/* must underflow */
+#endif // 0
+}
+
+static void test_parse()
+{
+	test_parse_null();
+	test_parse_true();
+	test_parse_false();
+	test_parse_number();
+	test_parse_expect_value();
+	test_parse_invalid_value();
+	test_root_not_singular();
+	test_parse_number_too_big();
 }
 
 int main()
 {
-	testParse();
+	test_parse();
 	printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
 	system("pause");
 	return main_ret;
